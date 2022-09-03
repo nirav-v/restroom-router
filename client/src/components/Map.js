a33fd5e2e41e3472b3ffd3aae6995f0b0b756f9d
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-09-02 17:36:10 -0700

    got marker element from react-map-gl package  to render on mapbox map using hard-coded coordinates

--- client/src/components/Map.js
+++ client/src/components/Map.js
@@ -1,17 +1,18 @@
 import React, { useRef, useEffect, useState } from "react";
 import "mapbox-gl/dist/mapbox-gl.css";
 import mapboxgl from "!mapbox-gl"; // eslint-disable-line import/no-webpack-loader-syntax
-
-import {NEARBY_RESTROOMS} from '../util/queries'
+import { style } from "@mui/system";
+import { Marker } from "react-map-gl";
 
 mapboxgl.accessToken =
+  process.env.REACT_APP_MAPBOX ||
   "pk.eyJ1Ijoibmlydjk5IiwiYSI6ImNsN2pwenFraTBpdHUzb216bnZ2eG5ndG0ifQ.3C0GFB4RKgl-KKzKPDns0Q";
 
 export default function Map() {
   const mapContainer = useRef(null);
   const map = useRef(null);
-  const [lng, setLng] = useState(-70.9);
-  const [lat, setLat] = useState(42.35);
+  const [lng, setLng] = useState(-117.1611);
+  const [lat, setLat] = useState(32.7157);
   const [zoom, setZoom] = useState(9);
 
   useEffect(() => {
@@ -22,6 +23,10 @@ export default function Map() {
       center: [lng, lat],
       zoom: zoom,
     });
+    // Create a default Marker and add it to the map.
+    const marker1 = new mapboxgl.Marker({ color: "black" })
+      .setLngLat([-117.1611, 32.7157])
+      .addTo(map.current);
   });
 
   useEffect(() => {
@@ -38,23 +43,33 @@ export default function Map() {
   };
 
   const sidebarStyle = {
-    backgroundColor: 'rgba(35, 55, 75, 0.9)',
+    backgroundColor: "rgba(35, 55, 75, 0.9)",
     color: "#fff",
     padding: "6px 12px",
-    fontFamily: 'monospace',
+    fontFamily: "monospace",
     ZIndex: 1,
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     margin: "12px",
     borderRadius: "4px",
   };
+
+  const markerStyle = {
+    backgroundImage: "url('mapbox-icon.png')",
+    backgroundSize: "cover",
+    width: "50px",
+    height: "50px",
+    borderRadius: "50%",
+    cursor: "pointer",
+  };
+
   return (
     <div>
       <div className="sidebar" style={sidebarStyle}>
         Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
       </div>
-      <div ref={mapContainer} className="map-container" style={mapStyle}/>
+      <div ref={mapContainer} className="map-container" style={mapStyle} />
     </div>
   );
 }
