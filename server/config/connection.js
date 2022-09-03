803c6daf9b5cc0a6233062759b5b70cfcb0285c4
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-09-03 13:40:32 -0700

    deploying to heroku

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose
-  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/bowelMvmtDB', {
+  .connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/restroom-routerDB', {
     useNewUrlParser: true,
     useUnifiedTopology: true
   })
