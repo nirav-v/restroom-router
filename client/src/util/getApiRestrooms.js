b1b633409268e1089693534b3323f64eea575c70
Nirav Venkatesan <niravvenkatesan@gmail.com>
2022-09-17 12:20:40 -0700

    moved apiRestroom fetch function into client utils folder since it will be imported into the seeds script

--- client/src/util/getApiRestrooms.js
+++ client/src/util/getApiRestrooms.js
@@ -1,4 +1,5 @@
-export function getRestroomApi() {
+export default function getRestroomApi() {
+
   var requestUrl =  'https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=false&unisex=false&lat=37.3387&lng=-121.8853';
 
   fetch(requestUrl)
@@ -6,6 +7,6 @@ export function getRestroomApi() {
       return response.json();
     })
     .then(function (data) {
-      console.log(data)
+      let apiRestrooms = data
     });
  }
